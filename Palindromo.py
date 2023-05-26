a8943ace26976d9970cd594cdca6b73e4ac1e477
Wesley <Wesleyfonsecaengp@gmail.com>
2023-05-26 10:40:22 -0300

    Adicionando o exercicio de palindromo

---
+++ Palindromo.py
@@ -0,0 +1,11 @@
+frase = str(input('Digite seu nome: ')).strip().upper()
+palavras = frase.split()
+junto = ' '.join(palavras)
+inverso = ''
+for letra in range(len(junto) -1,-1,-1):
+   inverso += junto[letra]
+print(f'O inverso de {junto} é o {inverso}')
+if inverso == junto:
+    print('Temos um palindromo')
+else:
+    print('Não temos um palindromo')
